158d12e1c04a7aa32deb1aea24cbba88783f900b
alaa alassi <alaa.alassi@nobleo.nl>
2022-01-05 00:18:20 +0100

    format code

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,7 +4,6 @@
 
 /* Implementation of class "MessageQueue" */
 
-
 template <typename T>
 T MessageQueue<T>::receive()
 {
@@ -12,7 +11,8 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics.
     // The received object should then be returned by the receive function.
     std::unique_lock<std::mutex> uLock(_mux);
-    _cond.wait(uLock,[this]{return !_message.empty();});
+    _cond.wait(uLock, [this]
+               { return !_message.empty(); });
     T msg = std::move(_message.back());
     _message.clear();
     return msg;
@@ -28,9 +28,7 @@ void MessageQueue<T>::send(T &&msg)
     _cond.notify_one();
 }
 
-
 /* Implementation of class "TrafficLight" */
-
 
 TrafficLight::TrafficLight()
 {
@@ -44,9 +42,11 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop
     // runs and repeatedly calls the receive function on the message queue.
     // Once it receives TrafficLightPhase::green, the method returns.
-    while(true){
+    while (true)
+    {
         auto trafficLightPhase = queue->receive();
-        if (trafficLightPhase == TrafficLightPhase::green){
+        if (trafficLightPhase == TrafficLightPhase::green)
+        {
             return;
         }
     }
@@ -62,7 +62,7 @@ void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
     //threads.emplace_back;
-    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases,this));
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -73,29 +73,32 @@ void TrafficLight::cycleThroughPhases()
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds.
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
     std::random_device dev;
-     std::mt19937 gen(dev());
+    std::mt19937 gen(dev());
     int minDuration = 4000;
     int maxDuration = 6000;
-    std::uniform_int_distribution<> dis(minDuration,maxDuration);
+    std::uniform_int_distribution<> dis(minDuration, maxDuration);
     auto tInit = std::chrono::steady_clock::now();
     auto tStop = std::chrono::milliseconds(minDuration);
-    while(true){
+    while (true)
+    {
         auto deltaT = std::chrono::steady_clock::now() - tInit;
-        if(deltaT >= tStop){
-            if(_currentPhase == TrafficLightPhase::green){
-            _currentPhase = TrafficLightPhase::red;
-            } else
+        if (deltaT >= tStop)
+        {
+            if (_currentPhase == TrafficLightPhase::green)
             {
-            _currentPhase = TrafficLightPhase::green;
+                _currentPhase = TrafficLightPhase::red;
+            }
+            else
+            {
+                _currentPhase = TrafficLightPhase::green;
             };
             auto message = _currentPhase;
             queue->send(std::move(message));
             deltaT = std::chrono::milliseconds(0);
             tInit = std::chrono::steady_clock::now();
             int temp = dis(gen);
-            tStop =  std::chrono::milliseconds(temp);
+            tStop = std::chrono::milliseconds(temp);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
-
 }
